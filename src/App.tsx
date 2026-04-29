a0b591967360ee46ebad7ff4032f25c3b8d7d9e2
Seenupsr2020 <seenupsr2020@gmail.com>
2026-04-30 00:10:13 +0530

    debug: node version and root render test

--- src/App.tsx
+++ src/App.tsx
@@ -10,15 +10,17 @@ import Header from './components/Header';
 
 export default function App() {
   return (
-    <Router>
-      <div className="flex flex-col min-h-screen bg-white">
-        <h1 style={{ position: 'fixed', top: 0, left: 0, zIndex: 9999, color: 'red' }}>TEST RENDER</h1>
-        <Header />
-        <main className="flex-grow">
-          <Home />
-        </main>
-      </div>
-    </Router>
+    <>
+      <h1 style={{ position: 'fixed', top: 0, left: 0, zIndex: 9999, color: 'red' }}>TEST RENDER</h1>
+      <Router>
+        <div className="flex flex-col min-h-screen bg-white">
+          <Header />
+          <main className="flex-grow">
+            <Home />
+          </main>
+        </div>
+      </Router>
+    </>
   );
 }
 
