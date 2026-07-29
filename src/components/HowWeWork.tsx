990899bf6804526e7d5d988b2750d23c8627450f
Seenupsr2020 <seenupsr2020@gmail.com>
2026-07-29 13:32:10 +0530

    Increase mobile font sizes further for About Us page

--- src/components/HowWeWork.tsx
+++ src/components/HowWeWork.tsx
@@ -61,7 +61,7 @@ const HowWeWork: React.FC = () => {
                     className="global-content-style"
                     style={{
                       color: '#333',
-                      fontSize: '110px',
+                      fontSize: '130px',
                       lineHeight: '1.6',
                       marginTop: '30px',
                       maxWidth: '450px'
@@ -149,7 +149,7 @@ const HowWeWork: React.FC = () => {
                   className="global-content-style"
                   style={{
                     color: '#333',
-                    fontSize: '110px',
+                    fontSize: '130px',
                     textAlign: 'center',
                     marginTop: '280px',
                     lineHeight: '1.4',
@@ -363,7 +363,7 @@ const ProcessCardFixed: React.FC<{ bg: string; color: string; title: string; hei
       zIndex: 2
     }}
   >
-    <span className="global-content-style" style={{ color, textAlign: 'center', fontSize: '96px' }}>{title}</span>
+    <span className="global-content-style" style={{ color, textAlign: 'center', fontSize: '110px' }}>{title}</span>
   </motion.div>
 );
 
