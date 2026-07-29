dfb1312940eaf4904a85d9f41c9a3eb29ea15b82
Seenupsr2020 <seenupsr2020@gmail.com>
2026-07-29 13:39:09 +0530

    Set mobile font sizes to match About Us paragraph size exactly

--- src/components/HowWeWork.tsx
+++ src/components/HowWeWork.tsx
@@ -61,7 +61,7 @@ const HowWeWork: React.FC = () => {
                     className="global-content-style"
                     style={{
                       color: '#333',
-                      fontSize: '160px',
+                      fontSize: '50px',
                       lineHeight: '1.6',
                       marginTop: '30px',
                       maxWidth: '450px'
@@ -149,7 +149,7 @@ const HowWeWork: React.FC = () => {
                   className="global-content-style"
                   style={{
                     color: '#333',
-                    fontSize: '160px',
+                    fontSize: '50px',
                     textAlign: 'center',
                     marginTop: '280px',
                     lineHeight: '1.4',
@@ -363,7 +363,7 @@ const ProcessCardFixed: React.FC<{ bg: string; color: string; title: string; hei
       zIndex: 2
     }}
   >
-    <span className="global-content-style" style={{ color, textAlign: 'center', fontSize: '130px' }}>{title}</span>
+    <span className="global-content-style" style={{ color, textAlign: 'center', fontSize: '50px' }}>{title}</span>
   </motion.div>
 );
 
